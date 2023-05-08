2bc4d8c94200a5c1ca05d58525bd27dc96359bef
MisHti03 <bhartithakur0186@gmail.com>
2023-05-08 11:46:23 +0530

    day12 complete

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class  CatalogModel{
+  static final items = [
+    Item(
+      id: 1,
+      name: "Bharti",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://m.media-amazon.com/images/I/31jQ91XUDhS._SY445_SX342_QL70_FMwebp_.jpg",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -10,7 +24,8 @@ class Item {
 }
 
 final products = [
-  Item(id:"Bharti01",
+  Item(
+  id: 1,
   name:"Bharti",
   desc:"Apple iPhone 12th generation",
   price:999,
